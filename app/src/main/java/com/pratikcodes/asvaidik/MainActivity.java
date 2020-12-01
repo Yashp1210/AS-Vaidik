cb0c414d9bbc592d8f295a734397ae40f256599c
pratikkate3 <pratikrkate@gmail.com>
2020-12-01 19:29:02 +0530

    HomeScreen/NavBar Done

--- app/src/main/java/com/pratikcodes/asvaidik/MainActivity.java
+++ app/src/main/java/com/pratikcodes/asvaidik/MainActivity.java
@@ -2,19 +2,58 @@ package com.pratikcodes.asvaidik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pratikcodes.asvaidik.fragments.HomeFragment;
+import com.pratikcodes.asvaidik.fragments.ProfileFragment;
 
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView homenav;
+
+    BottomNavigationView nav;
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        homenav = findViewById(R.id.homenav);
 
+        nav = findViewById(R.id.navhome);
+        fab = findViewById(R.id.fab_req);
+
+        HomeFragment fragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.screen,fragment).commit();
+
+        nav.setOnNavigationItemSelectedListener(listener);
+
+        fab.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(),ReqActivity.class);
+            startActivity(intent);
+        });
 
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener listener = item -> {
+
+        switch(item.getItemId())
+        {
+            case R.id.home:
+                HomeFragment home = new HomeFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.screen,home).commit();
+                break;
+
+            case R.id.add:
+                Intent intent = new Intent(getApplicationContext(),ReqActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.profile:
+                ProfileFragment profile = new ProfileFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.screen,profile).commit();
+                break;
+        }
+        return true;
+    };
 }
---
+++ app/src/main/java/com/pratikcodes/asvaidik/fragments/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.pratikcodes.asvaidik.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pratikcodes.asvaidik.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
