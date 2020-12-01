794f987e1a93481b7645ac2a000729ca9a1fb2ee
pratikkate3 <pratikrkate@gmail.com>
2020-12-01 17:45:33 +0530

    Start(1)

--- app/src/main/java/com/pratikcodes/asvaidik/MainActivity.java
+++ app/src/main/java/com/pratikcodes/asvaidik/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView homenav
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
