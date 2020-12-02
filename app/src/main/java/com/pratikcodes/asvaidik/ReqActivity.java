e8f0208885bebfb1e49cdddbfd2e05505fd58de9
pratikkate3 <pratikrkate@gmail.com>
2020-12-02 11:03:43 +0530

    commit

--- app/src/main/java/com/pratikcodes/asvaidik/ReqActivity.java
+++ app/src/main/java/com/pratikcodes/asvaidik/ReqActivity.java
@@ -4,15 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class ReqActivity extends AppCompatActivity {
 
-    EditText name,email,phone,location,description;
+    EditText ename,eemail,ephone,elocation,edescription;
     CheckBox android,web,pooja,poojaAccesories,havanSamgri,pandits,astrologer;
     Button next;
     Toolbar tool;
@@ -23,72 +24,30 @@ public class ReqActivity extends AppCompatActivity {
         tool = findViewById(R.id.reqtool);
         setSupportActionBar(tool);
 
-        name = findViewById(R.id.cname);
-        email = findViewById(R.id.cmail);
-        phone = findViewById(R.id.cphone);
-        location = findViewById(R.id.location);
-        description = findViewById(R.id.description);
+        //text fields
+        ename = findViewById(R.id.cname);
+        eemail = findViewById(R.id.cmail);
+        ephone = findViewById(R.id.cphone);
+        elocation = findViewById(R.id.location);
+        edescription = findViewById(R.id.description);
 
+        //checkboxes for type
         android = findViewById(R.id.check_androidapp);
         web = findViewById(R.id.check_website);
 
+        //checkboxes for category
         pooja = findViewById(R.id.pooja);
         poojaAccesories = findViewById(R.id.poojaacces);
         havanSamgri = findViewById(R.id.havan);
         pandits = findViewById(R.id.pandit);
         astrologer = findViewById(R.id.astrologer);
 
+        //button
         next = findViewById(R.id.nextbtn);
-
-        String type = "";
 
         next.setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(),SubmitActivity.class);
-            intent.putExtra("cname",name.getText());
-            intent.putExtra("cemail",email.getText());
-            intent.putExtra("cphone",phone.getText());
-            intent.putExtra("clocation",location.getText());
-            intent.putExtra("cdesc",description.getText());
-
-
-            if(android.isChecked())
-            {
-                type.concat("Android App\n");
-            }
-
-            if(web.isChecked())
-            {
-                type.concat("Website\n");
-            }
-
-            if(pooja.isChecked())
-            {
-                intent.putExtra("cpooja","Pooja");
-            }
-
-            if(poojaAccesories.isChecked())
-            {
-                intent.putExtra("cpoojaaccesories","Pooja Accessories");
-            }
-
-            if(havanSamgri.isChecked())
-            {
-                intent.putExtra("chavan","Havan Samagri");
-            }
-
-            if(pandits.isChecked())
-            {
-                intent.putExtra("cpandit","Pandits");
-            }
-
-            if(astrologer.isChecked())
-            {
-                intent.putExtra("castrologer","astrologer");
-            }
-
-            intent.putExtra("ctype",type);
             startActivity(intent);
-
         });
     }
 }
--- app/src/main/java/com/pratikcodes/asvaidik/SubmitActivity.java
+++ app/src/main/java/com/pratikcodes/asvaidik/SubmitActivity.java
@@ -2,33 +2,16 @@ package com.pratikcodes.asvaidik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+
 
 public class SubmitActivity extends AppCompatActivity {
 
-    TextView name,mail,phone,location,type,catagory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
 
-        name = findViewById(R.id.display_name);
-        mail = findViewById(R.id.display_mail);
-        phone = findViewById(R.id.display_phone);
-        location = findViewById(R.id.display_location);
-        type = findViewById(R.id.display_type);
-        catagory = findViewById(R.id.display_catagory);
-
-        Intent intent = getIntent();
-        String displayname = intent.getStringExtra("cname");
-        name.setText(displayname);
-
-//        mail.setText(intent.getStringExtra("cemail"));
-//        phone.setText(intent.getStringExtra("cphone"));
-//        location.setText(intent.getStringExtra("clocation"));
-//        type.setText(intent.getStringExtra("ctype"));
-        //catagory.setText(intent.getStringExtra("ccatagory"));
     }
 }
